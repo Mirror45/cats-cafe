f29bf82d7b185f51322ed1eac82c32c6ccac423c
Mirror <yllikano@gmail.com>
2024-07-30 17:15:27 +0300

    Опишем тему приложения и глобальные стили

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,15 @@
+import React from "react";
+import PageWrapper from "/src/components/layout/page-wrapper/page-wrapper";
+import starList from "/src/mocks/starList";
+import { GlobalStyle } from "./styled";
+
 export default function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyle />
       <h1>Start Project</h1>
       <h2>Start editing to see some magic happen!</h2>
-    </div>
+    </>
   );
 }
+
