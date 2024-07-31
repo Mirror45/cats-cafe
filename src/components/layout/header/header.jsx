7c165fb94590f6c6628e979131fc1e58aa4e2fb2
Mirror <yllikano@gmail.com>
2024-07-31 12:28:31 +0300

    Добавление компонентов: header/logo/nav

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "/src/components/ui/logo/logo";
+import Nav from "/src/components/layout/nav/nav";
+import { StyledSection } from "./styled";
 
-function Header() {
-  return <></>;
+function Header({ pageUrl }) {
+  return (
+    <StyledSection as="header">
+      <Logo />
+      <Nav pageUrl={pageUrl} />
+    </StyledSection>
+  );
 }
 
 export default Header;
--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MainPage from "/src/components/pages/main-page";
 import { Main } from "./styled";
-
+import Header from "/src/components/layout/header/header";
 // Обёртка для контента страниц
 function PageWrapper({ children, ...prop }) {
   return (
     <>
+      <Header />
       <Main>
         <MainPage {...prop} />
       </Main>
