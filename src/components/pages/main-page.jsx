7fb13b7281e387436f04f1ced318ddef0a7e0750
Mirror <yllikano@gmail.com>
2024-08-02 18:52:38 +0300

    Добавляем компоненты: stars-list / star-card/ star-icon

--- src/components/pages/main-page.jsx
+++ src/components/pages/main-page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "/src/components/blocks/about/about";
+import StarsList from "/src/components/blocks/stars-list/stars-list";
 import { TitleLevel } from "/src/components/ui/title/title";
 
 function MainPage({ stars }) {
   return (
     <>
       <About level={TitleLevel.H1} />
+      <StarsList stars={stars} level={TitleLevel.H2} />
     </>
   );
 }
