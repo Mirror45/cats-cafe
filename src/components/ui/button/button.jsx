5c984a3e679289303b52084da2264b64e0c80834
Mirror <yllikano@gmail.com>
2024-08-15 00:35:54 +0300

    Добавляем проверки для компонента buy на случай если придут пустые массивы данных

--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -1,12 +1,12 @@
 import React, { forwardRef } from "react";
 import { StyledButton } from "./styled";
 
-const Button = forwardRef(({ children, minWidth, link, className }, ref) => {
+const Button = forwardRef(({ children, minWidth, link, className, onClick }, ref) => {
   return (
     <StyledButton
       ref={ref}
       $minWidth={minWidth}
-      {...(link ? { href: link } : { as: "button", type: "button" })}
+      {...(link ? { href: link } : { as: "button", onClick, type: "button" })}
       className={className}
     >
       {children}
